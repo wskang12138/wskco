f6452412b00eeda04facec15a18e8fdc58ba8354
wskang12138 <185331645@qq.com>
2023-04-04 09:28:01 +0800

    fix: 更换路由模式

--- .umirc.ts
+++ .umirc.ts
@@ -14,7 +14,9 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  hash: true,
+  history: {
+    type: 'hash', //采用hash模式刷新路由不会丢失
+  },
   // Because of using GitHub Pages
   base: `/${repo}/`,
   publicPath: process?.env?.NODE_ENV === 'development' ? `/${repo}/` : './',
@@ -34,5 +36,6 @@ export default defineConfig({
       },
     ],
   },
+  exportStatic: {} // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // more config: https://d.umijs.org/config
 });

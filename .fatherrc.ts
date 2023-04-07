539366522308d8240f437dc09f48ebb4564f8e57
wskang12138 <185331645@qq.com>
2023-04-07 16:43:39 +0800

    fix: 发布打包路径的文件问题

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,9 +1,6 @@
-export default {
-  target: 'browser',
-  entry: 'src/index.ts',
-  //   esm: {},
-  //   cjs: {},
-  autoprefixer: {
-    browsers: ['ie>9', 'Safari >= 6'],
-  },
-};
+import { defineConfig } from 'father';
+
+export default defineConfig({
+  // more father config: https://github.com/umijs/father/blob/master/docs/config.md
+  esm: { output: 'dist' }, //输出路径
+});
